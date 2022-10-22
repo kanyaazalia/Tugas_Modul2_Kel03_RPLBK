d578d659a53964c5272dffd84fa3acb256ca6c01
Kanya Azalia Andriyani <kanyaazalia@gmail.com>
2022-10-22 19:53:24 +0700

    Bisa OnKeyUp
    Belum nambahin button.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DataMahasiswa from "./component/Data.js";
+import Card from "./component/Card.jsx";
+import React from "react";
 
-function App() {
+export default function App() {
+  const [data, setData] = React.useState([]);
+  const [state, setState] = React.useState(false);
+
+  const findData = (e) => {
+    const newData = DataMahasiswa.filter((mahasiswa) =>
+      mahasiswa.nama_lengkap.toLowerCase().includes(e.toLowerCase())
+    );
+    e === "" ? setData([]) : setData(newData);
+    e === "" ? setState(false) : setState(true);
+    console.log(newData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        "flex-wrap": "wrap",
+        "flex-direction": "column",
+        "align-content": "center",
+      }}
+    >
+      <h2 style={{ "align-self": "center" }}>Kelompok 03:</h2>
+      <input
+        onChange={(data) => findData(data.target.value)}
+        placeholder="Masukkan Nama Mahasiswa..."
+        style={{ width: 400 }}
+      />
+      <br />
+
+      {data.length !== 0 ? (
+        data.map((mahasiswa, i) => {
+          return <Card key={i} data={mahasiswa} i={i} />;
+        })
+      ) : state ? (
+        <h2 style={{ "align-self": "center" }}>Data Tidak Ditemukan</h2>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
-
-export default App;
