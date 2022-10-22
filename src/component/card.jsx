7dd85c8dd9247d02514c4909b05aba2382fdadc0
afifa <afifahumaira1@gmail.com>
2022-10-22 12:29:41 +0700

    commit afifa

---
+++ src/component/card.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+export default function Card(props) {
+  return (
+    <div style={{'background-color': `${props.i % 2 === 1 ? '#e6ca2e' : '#cc752e'}`, 'margin-bottom': 10, 'width': 398, 'border-radius': 10, 'padding':5}}>
+      <table>
+        <tbody>
+          <tr>
+            <td>Nama Lengkap</td>
+            <td>:</td>
+            <td>{props.data.nama_lengkap}</td>
+          </tr>
+          <tr>
+            <td>Nama Panggilan</td>
+            <td>:</td>
+            <td>{props.data.nama_panggilan}</td>
+          </tr>
+          <tr>
+            <td>NIM</td>
+            <td>:</td>
+            <td>{props.data.nim}</td>
+          </tr>
+          <tr>
+            <td>Nomor Tlp</td>
+            <td>:</td>
+            <td>{props.data.nomor_telepon}</td>
+          </tr>
+          <tr>
+            <td>Id Line</td>
+            <td>:</td>
+            <td>{props.data.id_line}</td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td>:</td>
+            <td>{props.data.email}</td>
+          </tr>
+          <tr>
+            <td>Hobi</td>
+            <td>:</td>
+            <td>{props.data.hobi}</td>
+          </tr>
+          <tr>
+            <td>Tanggal Lahir</td>
+            <td>:</td>
+            <td>{props.data.tanggal_lahir === undefined || props.data.tanggal_lahir === null ? '-' : props.data.tanggal_lahir}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
